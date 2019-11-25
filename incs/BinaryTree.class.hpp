e6629d8917a7f2b296dc7c4b50754867ab3f7829
Unknown <lmartin-@student.42.fr>
2019-11-25 17:11:03 +0100

    Binary tree

---
+++ incs/BinaryTree.class.hpp
@@ -0,0 +1,35 @@
+#ifndef BINARY_TREE_CLASS_HPP
+# define BINARY_TREE_CLASS_HPP
+
+# include <iostream>
+using namespace std;
+
+
+
+template <class T>
+class BinaryTree
+{
+	private:
+		struct _node
+		{
+			T *data;
+			struct _node *left;
+			struct _node *right;
+		};
+
+		_node* _root;
+
+
+	public:
+
+	    BinaryTree();
+	    ~BinaryTree();
+
+		void freeNodes(_node* _node);
+		void add(T *var);
+		void replace(_node *replaced);
+		bool isInTree(T *var);
+		void remove(T *var);
+};
+
+#endif
